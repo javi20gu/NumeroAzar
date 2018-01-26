c7400ec5d3d75b69d5c5d198ed539c007df4b4ec
javier <33096723+javi20gu@users.noreply.github.com>
2018-01-26 22:35:59 +0100

    Modulo Base
    + Optimización

--- main.py
+++ main.py
@@ -1,38 +1,41 @@
 from sys import exit
 from random import randint
-from __init__.funcion1 import *
-from __init__.funcion2 import *
-from __init__.setting import *
+from submodulos import *
+from setting import INTENTOS
 
-print()
-print(format("BIENVENIDO","-^75"))
-print(format("ADIVINE EL NUMERO","-^75"))
-print()
-texto = setText()
-rango1, rango2 = setRango()
-numero_aleatorio = randint(rango1, rango2)
-print(f"Sus rangos son: {rango1} al {rango2}")
+def main():
 
-while INTENTOS > 0:
-    print(f"Intentos Restantes: {INTENTOS}")
-    try:
-        numero = int(input(f"{texto}"))
-    except ValueError:
-        print("Error: Tiene que ser un numero")
+    global INTENTOS
 
-    if numero_aleatorio < numero and INTENTOS != 1:
-        print("Demasiado Grande")
-        INTENTOS -= 1
-    elif numero_aleatorio > numero and INTENTOS != 1:
-        print("Demasiado Pequeño")
-        INTENTOS -= 1
-    elif numero == numero_aleatorio:
-        print(f"Has Ganado en el {INTENTOS} intento")
-        input("Pulse enter para continuar...")
-        exit()
-    elif numero != numero_aleatorio and INTENTOS == 1:
-        print()
-        print(format("GAME OVER","-^75"))
-        INTENTOS -= 1
-        input("Pulse enter para continuar...")
-        exit()
+    print()
+    print(format("BIENVENIDO","-^75"))
+    print(format("ADIVINE EL NUMERO","-^75"))
+    print()
+    texto = setText()
+    rango1, rango2 = setRango()
+    numero_aleatorio = randint(rango1, rango2)
+    print(f"Sus rangos son: {rango1} al {rango2}")
+
+    while INTENTOS > 0:
+        print(f"Intentos Restantes: {INTENTOS}")
+        try:
+            numero = int(input(f"{texto}"))
+
+            if numero_aleatorio < numero and INTENTOS != 1:
+                print("Demasiado Grande")
+                INTENTOS -= 1
+            elif numero_aleatorio > numero and INTENTOS != 1:
+                print("Demasiado Pequeño")
+                INTENTOS -= 1
+            elif numero == numero_aleatorio:
+                print(f"Has Ganado en el {INTENTOS} intento")
+                input("Pulse enter para continuar...")
+                exit()
+            elif numero != numero_aleatorio and INTENTOS == 1:
+                print()
+                print(format("GAME OVER","-^75"))
+                INTENTOS -= 1
+                input("Pulse enter para continuar...")
+                exit()
+        except (ValueError, NameError):
+            print("Error: Tiene que ser un numero")
